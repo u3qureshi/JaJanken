ee51a87dc40611bd293af63dc9422a1038be2784
Usman <usman-qureshi@outlook.com>
2022-04-21 07:53:30 -0400

    Create index.js file and create functioning game with the UI

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-
-
-let playMove;
-let comPlay;
 let comPoint = 0;
 let playPoint = 0;
 
-fullJankenGame();
+const rockButton = document.querySelector('#icons>.rock-button');
+const paperButton = document.querySelector('#icons>.paper-button');
+const scissorsButton = document.querySelector('#icons>.scissors-button');
+const resultText1 = document.querySelector('.result-text1');
+const resultText2 = document.querySelector('.result-text2');
 
 
-function fullJankenGame(){
-    console.log("Let's play Rock, Paper, Scissors! First to five points is the winner.");
+rockButton.addEventListener('click', () => {
+    playJankenRound('Rock');
+});
+paperButton.addEventListener('click', () => {
+    playJankenRound('Paper');
+});
+scissorsButton.addEventListener('click', () => {
+    playJankenRound('Scissors');
+});
 
-    while(playPoint<5 && comPoint<5){
-        playMove = playerMove();
-        comPlay = computerMove();
 
-        playJankenRound();
+function playJankenRound(playMove = '') {
+    let string = '';
+    let string2 = '';
+    let comPlay = computerMove();
+
+
+
+    if (playMove == comPlay) {
+        if (playMove == 'Rock') {
+            string = 'You both chose ✊'
+            string2 = "It's a tie"
+        } else if (playMove == 'Paper') {
+            string = 'You both chose ✋'
+            string2 = "It's a tie"
+        } else {
+            string = 'You both chose ✌'
+            string2 = "It's a tie"
+        }
+        resultText1.innerText = string;
+        resultText2.innerText = string2;
+        return;
+    }
+    if (playMove == 'Rock' && comPlay == 'Scissors') {
+        string = `Your move: ✊      Gon's Move: ✌`;
+        string2 = 'Nice Work!';
+        playPoint++;
+    } else if (playMove == 'Rock' && comPlay == 'Paper') {
+        string = `Your move: ✊      Gon's Move: ✋`;
+        string2 = 'Try Again.'
+        comPoint++;
+    } else if (playMove == 'Paper' && comPlay == 'Scissors') {
+        string = `Your move: ✋      Gon's Move: ✌`;
+        string2 = 'Try Again.'
+        comPoint++;
+    } else if (playMove == 'Paper' && comPlay == 'Rock') {
+        string = `Your move: ✋      Gon's Move: ✊`;
+        string2 = 'Nice Work!';
+        playPoint++;
+    } else if (playMove == 'Scissors' && comPlay == 'Rock') {
+        string = `Your move: ✌      Gon's Move: ✊`;
+        string2 = 'Try Again.'
+        comPoint++;
+    } else if (playMove == 'Scissors' && comPlay == 'Paper') {
+        string = `Your move: ✌      Gon's Move: ✋`;
+        string2 = 'Nice Work!';
+        playPoint++;
     }
 
-    if(playPoint == 5) console.log('Congratulations, you won the Janken Game!')
-    else console.log('Sorry you lost, try again.')
+    resultText1.innerText = string;
+    resultText2.innerText = string2;
 }
 
-function playJankenRound(){
-    let string='';
 
-    if (playMove==comPlay) string = `It is a draw. You both picked ${playMove}`
-    if (playMove=='Rock' && comPlay=='Scissors') {
-        string = 'You Win! Rock beats Scissors';
-        playPoint++;
-    }
-    else if (playMove=='Rock' && comPlay=='Paper') {
-        string = 'You Lose! Paper beats Rock';
-        comPoint++;
-    }
-    else if (playMove=='Paper' && comPlay=='Scissors'){
-        string = 'You Lose! Scissors beats Paper'
-        comPoint++;
-    }
-    else if (playMove=='Paper' && comPlay=='Rock'){
-        string = 'You Win! Paper beats Rock'
-        playPoint++;
-    }
-    else if (playMove=='Scissors' && comPlay=='Rock'){
-        string = 'You Lose! Rock beats Scissors'
-        comPoint++;
-    }
-    else if (playMove=='Scissors' && comPlay=='Paper'){
-        string = 'You Win! Scissors beats Paper'
-        playPoint++;
-    }
-
-    console.log(`The computer selected ${comPlay}`)
-    console.log(string)
-}
-
-
-function computerMove(){
+/* This function returns a String of 'Rock', 'Paper', or 'Scissors' */
+function computerMove() {
     let random = Math.floor(Math.random() * 3); //math.random returns a double
     if (random == 1) return 'Rock';
     else if (random == 2) return 'Paper';
     else return 'Scissors';
-}
-
-function playerMove(){
-    let number = parseInt(window.prompt("Enter a number between 1 to 3./nRock = 1/nPaper = 2/nScissors = 3"), 10);
-    let move = "";
-    if (number == 1) move='Rock';
-    else if (number == 2) move='Paper';
-    else if (number == 3) move='Scissors';
-    else alert('That is not a valid input.');
-
-    return move;
 }
